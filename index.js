5498d7a2c4bcad3c807589b3b1a862039c75f80b
Ahmad Reza <ahmad.alkhwarizmi@gmail.com>
2018-06-18 00:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Ahmad Reza"
-var height = "54"
+var height = 54
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
